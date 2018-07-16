631bae88d710f6d27ee1d0245164b183205a7bf7
sangmin <sang93423@gmail.com>
2018-07-16 16:03:17 +0900

    dev.py 버킷 이름 추가해주게

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,12 +1,18 @@
 from .base import *
 
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'dev.json')))
+
 DEBUG = True
+ALLOWED_HOSTS = []
 
 WSGI_APPLICATION = 'config.wsgi.dev.application'
 
-ALLOWED_HOSTS = []
-
-secrets = json.load(open(os.path.join(SECRETS_DIR, 'dev.json')))
+INSTALLED_APPS += [
+    'storages',
+]
+# DB
 DATABASES = secrets['DATABASES']
 
-STATIC_URL = '/static/'
+# Media
+DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
